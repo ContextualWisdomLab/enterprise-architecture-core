07296a4377f9ea09da0fd75148746b3df869adc6
Seongho Bae <me@seonghobae.me>
2026-08-17 03:32:08 +0900

    test: enforce provenance-required truth and evidence tenant integrity

--- database/tests/zz_verify_evidence_contract.sql
+++ database/tests/zz_verify_evidence_contract.sql
@@ -21,6 +21,41 @@ INSERT INTO architecture_core.evidence_record (
     'semantic-data-portal:evidence-123'
 );
 
+INSERT INTO architecture_core.evidence_record (
+    tenant_record_id,
+    evidence_record_id,
+    evidence_uri,
+    sha256_digest,
+    source_locator
+) VALUES (
+    '0195d145-64e8-7f4f-8a23-a0cc784cb711',
+    '0195d145-64e8-7f4f-8a23-a0cc784cbf12',
+    'urn:cwl:tenant_001:semantic_data_portal:data_asset:0195d145-64e8-7f4f-8a23-a0cc784cb912',
+    repeat('b', 64),
+    'catalog:asset-912'
+);
+
+DO $$
+BEGIN
+  BEGIN
+    INSERT INTO architecture_core.evidence_record (
+        tenant_record_id,
+        evidence_record_id,
+        evidence_uri,
+        sha256_digest
+    ) VALUES (
+        '0195d145-64e8-7f4f-8a23-a0cc784cb711',
+        '0195d145-64e8-7f4f-8a23-a0cc784cbf13',
+        'urn:cwl:tenant_002:semantic_data_portal:data_asset:0195d145-64e8-7f4f-8a23-a0cc784cb913',
+        repeat('9', 64)
+    );
+    RAISE EXCEPTION 'foreign-tenant evidence URI unexpectedly accepted';
+  EXCEPTION
+    WHEN check_violation THEN NULL;
+  END;
+END;
+$$;
+
 DO $$
 DECLARE
   invalid_evidence_uri text;
@@ -37,7 +72,7 @@ BEGIN
     probe_index := probe_index + 1;
     probe_id := format(
         '0195d145-64e8-7f4f-8a23-a0cc784cbf%02s',
-        11 + probe_index
+        13 + probe_index
     )::uuid;
     BEGIN
       INSERT INTO architecture_core.evidence_record (
@@ -104,5 +139,117 @@ BEGIN
   END;
 END;
 $$;
+
+DO $$
+DECLARE
+  required_status text;
+  probe_index integer := 0;
+BEGIN
+  FOREACH required_status IN ARRAY ARRAY['authoritative', 'observed']
+  LOOP
+    probe_index := probe_index + 1;
+    BEGIN
+      INSERT INTO architecture_core.object_revision (
+          tenant_record_id,
+          object_revision_id,
+          architecture_object_id,
+          revision_number,
+          object_title,
+          valid_from,
+          valid_to,
+          truth_status_code
+      ) VALUES (
+          '0195d145-64e8-7f4f-8a23-a0cc784cb711',
+          format('0195d145-64e8-7f4f-8a23-a0cc784cbf3%s', probe_index)::uuid,
+          '0195d145-64e8-7f4f-8a23-a0cc784cb902',
+          30 + probe_index,
+          'Evidence-required revision',
+          format('203%s-01-01T00:00:00Z', probe_index)::timestamptz,
+          format('203%s-06-01T00:00:00Z', probe_index)::timestamptz,
+          required_status
+      );
+      RAISE EXCEPTION '% object revision without evidence unexpectedly accepted', required_status;
+    EXCEPTION
+      WHEN check_violation THEN NULL;
+    END;
+  END LOOP;
+END;
+$$;
+
+DO $$
+DECLARE
+  required_status text;
+  probe_index integer := 0;
+BEGIN
+  FOREACH required_status IN ARRAY ARRAY['authoritative', 'observed']
+  LOOP
+    probe_index := probe_index + 1;
+    BEGIN
+      INSERT INTO architecture_core.architecture_relation (
+          tenant_record_id,
+          architecture_relation_id,
+          relation_type_id,
+          source_object_id,
+          target_object_id,
+          valid_from,
+          valid_to,
+          truth_status_code
+      ) VALUES (
+          '0195d145-64e8-7f4f-8a23-a0cc784cb711',
+          format('0195d145-64e8-7f4f-8a23-a0cc784cbf4%s', probe_index)::uuid,
+          '0195d145-64e8-7f4f-8a23-a0cc784cb811',
+          '0195d145-64e8-7f4f-8a23-a0cc784cb902',
+          '0195d145-64e8-7f4f-8a23-a0cc784cb901',
+          format('204%s-01-01T00:00:00Z', probe_index)::timestamptz,
+          format('204%s-06-01T00:00:00Z', probe_index)::timestamptz,
+          required_status
+      );
+      RAISE EXCEPTION '% architecture relation without evidence unexpectedly accepted', required_status;
+    EXCEPTION
+      WHEN check_violation THEN NULL;
+    END;
+  END LOOP;
+END;
+$$;
+
+INSERT INTO architecture_core.object_revision (
+    tenant_record_id,
+    object_revision_id,
+    architecture_object_id,
+    revision_number,
+    object_title,
+    valid_from,
+    valid_to,
+    truth_status_code
+) VALUES (
+    '0195d145-64e8-7f4f-8a23-a0cc784cb711',
+    '0195d145-64e8-7f4f-8a23-a0cc784cbf50',
+    '0195d145-64e8-7f4f-8a23-a0cc784cb902',
+    50,
+    'Proposed evidence-optional revision',
+    '2050-01-01T00:00:00Z',
+    '2050-06-01T00:00:00Z',
+    'proposed'
+);
+
+INSERT INTO architecture_core.architecture_relation (
+    tenant_record_id,
+    architecture_relation_id,
+    relation_type_id,
+    source_object_id,
+    target_object_id,
+    valid_from,
+    valid_to,
+    truth_status_code
+) VALUES (
+    '0195d145-64e8-7f4f-8a23-a0cc784cb711',
+    '0195d145-64e8-7f4f-8a23-a0cc784cbf51',
+    '0195d145-64e8-7f4f-8a23-a0cc784cb811',
+    '0195d145-64e8-7f4f-8a23-a0cc784cb902',
+    '0195d145-64e8-7f4f-8a23-a0cc784cb901',
+    '2051-01-01T00:00:00Z',
+    '2051-06-01T00:00:00Z',
+    'inferred'
+);
 
 RESET ROLE;
